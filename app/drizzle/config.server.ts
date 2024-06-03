38681edda87a1f75f9a0957446c9139c9a881f11
Miči Hrešková <hroncovam@yahoo.com>
2024-06-03 15:54:31 +0100

    Adding drizzle\config.server.ts
    npm i --save-dev drizzle-kit @types/better-sqlite3

---
+++ app/drizzle/config.server.ts
@@ -0,0 +1,18 @@
+import { drizzle } from "drizzle-orm/better-sqlite3"
+import { migrate } from "drizzle-orm/better-sqlite3/migrator"
+import Database from "better-sqlite3"
+
+if (!process.env.DATABASE_PATH) {
+  throw new Error(
+    "Missing environment variable: DATABASE_PATH",
+  )
+}
+
+export const db = drizzle(
+  new Database(process.env.DATABASE_PATH),
+)
+
+// Automatically run migrations on startup
+void migrate(db, {
+  migrationsFolder: "app/drizzle/migrations",
+})
