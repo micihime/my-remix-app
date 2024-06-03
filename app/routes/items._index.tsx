b4f55904fa45bdb3fcf47021eaeb86e0d9765b9d
Miči Hrešková <hroncovam@yahoo.com>
2024-06-03 16:54:45 +0100

    Adding loader and mock data

--- app/routes/items._index.tsx
+++ app/routes/items._index.tsx
@@ -1,7 +1,34 @@
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+export const loader = async () => {
+  return json({
+    items: [
+      {
+        id: 1,
+        title: "My First Post",
+      },
+      {
+        id: 2,
+        title: "A Mixtape I Made Just For You",
+      },
+    ],
+  });
+};
+
 export default function Items() {
-    return (
-      <main>
-        <h1>Items</h1>
-      </main>
-    );
-  }
+  const { items } = useLoaderData<typeof loader>();
+
+  return (
+    <main>
+      <h1>Items</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+              {item.title}
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
