9300b92dfdfe648b82de41c48ba389845691ed8b
Miči Hrešková <hroncovam@yahoo.com>
2024-06-04 08:49:02 +0100

    Adding items, saving them in db
    https://www.jacobparis.com/content/remix-drizzle-sqlite

--- app/routes/items._index.tsx
+++ app/routes/items._index.tsx
@@ -1,34 +1,38 @@
-import { json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { db } from "~/drizzle/config.server";
+import { items } from "~/drizzle/schema.server";
 
-export const loader = async () => {
-  return json({
-    items: [
-      {
-        id: 1,
-        title: "My First Post",
-      },
-      {
-        id: 2,
-        title: "A Mixtape I Made Just For You",
-      },
-    ],
-  });
-};
-
-export default function Items() {
-  const { items } = useLoaderData<typeof loader>();
-
-  return (
-    <main>
-      <h1>Items</h1>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-              {item.title}
-          </li>
-        ))}
-      </ul>
-    </main>
-  );
-}
+export async function action({
+    request,
+  }: ActionFunctionArgs) {
+    db.insert(items).values({ title: "Item title" }).run()
+    return {
+      success: true,
+    }
+  }
+  export async function loader({
+    request,
+  }: LoaderFunctionArgs) {
+    // use drizzle to get the data
+    const data = db.select().from(items).all()
+    return json({
+      data,
+    })
+  }
+  export default function Index() {
+    const items = useLoaderData<typeof loader>()
+    return (
+      <div>
+        <h1> Items </h1>
+        <ul>
+          {items.data.map((item) => (
+            <li key={item.id}>{item.title}</li>
+          ))}
+        </ul>
+        <Form method="POST">
+          <input type="submit" value="Submit" />
+        </Form>
+      </div>
+    )
+  }
