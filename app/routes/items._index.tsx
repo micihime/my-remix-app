ca428b2df2194805778a28f0eddeb3e1abb9cf64
Miči Hrešková <hroncovam@yahoo.com>
2024-06-04 09:52:42 +0100

    Renaming index to items

--- app/routes/items._index.tsx
+++ app/routes/items._index.tsx
@@ -1,38 +1,42 @@
 import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
+
 import { db } from "~/drizzle/config.server";
 import { items } from "~/drizzle/schema.server";
 
 export async function action({
-    request,
-  }: ActionFunctionArgs) {
-    db.insert(items).values({ title: "Item title" }).run()
-    return {
-      success: true,
-    }
+  request,
+}: ActionFunctionArgs) {
+  console.log(request)
+  db.insert(items).values({ title: "Item title" }).run()
+  return {
+    success: true,
   }
-  export async function loader({
-    request,
-  }: LoaderFunctionArgs) {
-    // use drizzle to get the data
-    const data = db.select().from(items).all()
-    return json({
-      data,
-    })
-  }
-  export default function Index() {
-    const items = useLoaderData<typeof loader>()
-    return (
-      <div>
-        <h1> Items </h1>
-        <ul>
-          {items.data.map((item) => (
-            <li key={item.id}>{item.title}</li>
-          ))}
-        </ul>
-        <Form method="POST">
-          <input type="submit" value="Submit" />
-        </Form>
-      </div>
-    )
-  }
+}
+
+export async function loader({
+  request,
+}: LoaderFunctionArgs) {
+  // use drizzle to get the data
+  const data = db.select().from(items).all()
+  return json({
+    data,
+  })
+}
+
+export default function Items() {
+  const items = useLoaderData<typeof loader>()
+  return (
+    <div>
+      <h1> Items </h1>
+      <ul>
+        {items.data.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
+      <Form method="POST">
+        <input type="submit" value="Submit" />
+      </Form>
+    </div>
+  )
+}
