8e1e576476215712621c2d5d9a6ff1f6caf280b2
Miči Hrešková <hroncovam@yahoo.com>
2024-06-04 08:51:34 +0100

    Simplifying config server

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -2,14 +2,14 @@ import { drizzle } from "drizzle-orm/better-sqlite3"
 import { migrate } from "drizzle-orm/better-sqlite3/migrator"
 import Database from "better-sqlite3"
 
-if (!process.env.DATABASE_PATH) {
-  throw new Error(
-    "Missing environment variable: DATABASE_PATH",
-  )
-}
+// if (!process.env.DATABASE_PATH) {
+//   throw new Error(
+//     "Missing environment variable: DATABASE_PATH",
+//   )
+// }
 
 export const db = drizzle(
-  new Database("/data/sqlite.db"), // new Database(process.env.DATABASE_PATH),
+  new Database("/sqlite.db"), // new Database(process.env.DATABASE_PATH),
 )
 
 // Automatically run migrations on startup
