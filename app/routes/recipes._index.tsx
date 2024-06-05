a7c7f233e4e7013c0bf1f6b929d5a88a1ef804e3
Miči Hrešková <hroncovam@yahoo.com>
2024-06-05 13:19:37 +0100

    Removing the code to seed ingredients - only temporary helper

--- app/routes/recipes._index.tsx
+++ app/routes/recipes._index.tsx
@@ -7,23 +7,6 @@ import { drinks, ingredients } from "~/drizzle/schema.server";
 
 export async function loader({ request, }: LoaderFunctionArgs) {  
     //join
-
-    db.insert(ingredients).values({ name: "lotus", drinkId: 1 }).run()
-    db.insert(ingredients).values({ name: "jasmine", drinkId: 1 }).run()
-    db.insert(ingredients).values({ name: "rose", drinkId: 1 }).run()
-    db.insert(ingredients).values({ name: "orchid", drinkId: 1 }).run()
-    db.insert(ingredients).values({ name: "gladiolus", drinkId: 1 }).run()
-    db.insert(ingredients).values({ name: "marigold", drinkId: 2 }).run()
-    db.insert(ingredients).values({ name: "hibiscus", drinkId: 3 }).run()
-    db.insert(ingredients).values({ name: "buttercup", drinkId: 3 }).run()
-    db.insert(ingredients).values({ name: "lily", drinkId: 3 }).run()
-    db.insert(ingredients).values({ name: "saffron", drinkId: 3 }).run()
-    db.insert(ingredients).values({ name: "poppy", drinkId: 4 }).run()
-    db.insert(ingredients).values({ name: "primrose", drinkId: 4 }).run()
-    db.insert(ingredients).values({ name: "daisy", drinkId: 4 }).run()
-    db.insert(ingredients).values({ name: "iris", drinkId: 5 }).run()
-    db.insert(ingredients).values({ name: "hydrangea", drinkId: 5 }).run()
-
     const recipe = await db.select().from(drinks).leftJoin(ingredients, eq(drinks.id, ingredients.drinkId))
 
     return json({
