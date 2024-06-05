79d55b51c7fce6ef36a8b0bd0ef32d6b2d138c4c
Miči Hrešková <hroncovam@yahoo.com>
2024-06-05 15:25:54 +0100

    Adding customer overview - how many drinks they had

--- app/routes/orders._index.tsx
+++ app/routes/orders._index.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { and, asc, desc, eq, gte, lt, ne, not, sql } from "drizzle-orm";
+import { and, asc, desc, eq, gt, gte, lt, ne, not, sql } from "drizzle-orm";
 
 import { db } from "~/drizzle/config.server";
 import { drinks, customers, orders } from "~/drizzle/schema.server";
@@ -11,11 +11,23 @@ export async function loader({ request, }: LoaderFunctionArgs) {
     .from(orders)
     .leftJoin(customers, eq(customers.id, orders.customerId))
     .leftJoin(drinks, eq(drinks.id, orders.drinkId))
-    
+
   const ordersAll = await ordersAllSubquery.orderBy(asc(customers.id), desc(drinks.price))
 
+  const customerDrinkCount = await db.select({
+    customerId: customers.id,
+    customer: customers.name,
+    drinkCount: sql<number>`cast(count(${drinks.id}) as int)`,
+  })
+    .from(orders)
+        .leftJoin(customers, eq(customers.id, orders.customerId))
+        .leftJoin(drinks, eq(drinks.id, orders.drinkId))
+    .groupBy(customers.name)
+    .having(({ drinkCount: count }) => gt(count, 1))
+
   return json({
-    ordersAll
+    ordersAll,
+    customerDrinkCount
   })
 }
 
@@ -24,7 +36,14 @@ export default function Items() {
   
   return (
     <div>
-      <h1> Orders </h1>
+      <h1> Customer overview </h1>
+      <ul>
+        {data.customerDrinkCount.map(customer => (
+          <li key={customer.customerId}>{customer.customer} drank {customer.drinkCount} drinks</li>
+        ))}
+      </ul>
+
+      <h1> Order log </h1>
       <ul>
         {data.ordersAll.map(order => (
           <li key={order.customer_bartender_drinks.id}>{order.customers?.name} ordered {order.drinks?.name} for {order.drinks?.price}</li>
