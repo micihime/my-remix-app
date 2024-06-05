a4f5c66f46782b8f1db4ff3a702df7d7a43b0eba
Miči Hrešková <hroncovam@yahoo.com>
2024-06-05 12:53:58 +0100

    Practice selects added to people page

--- app/routes/people._index.tsx
+++ app/routes/people._index.tsx
@@ -1,29 +1,62 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
+import { sql } from "drizzle-orm";
 
 import { db } from "~/drizzle/config.server";
 import { people } from "~/drizzle/schema.server";
 
-export async function loader({
-  request,
-}: LoaderFunctionArgs) {
+export async function loader({ request, }: LoaderFunctionArgs) {
   // use drizzle to get the data
-  const data = db.select().from(people).all()
+  const peopleAll = db.select().from(people).all()
+
+  //Partial select
+  const peopleMin = await db.select({
+    field1: people.id,
+    field2: people.name,
+  }).from(people);
+  const { field1, field2 } = peopleMin[0];
+
+  //Using arbitrary expressions as selection fields
+  const bartendersUpper = await db.select({
+    id: people.id,
+    upperName: sql<string>`upper(${people.name})`,
+  }).from(people);
+
   return json({
-    data,
+    peopleAll,
+    peopleMin,
+    field1, 
+    field2,
+    bartendersUpper
   })
 }
 
 export default function People() {
-  const people = useLoaderData<typeof loader>()
+  const data = useLoaderData<typeof loader>()
   return (
     <div>
       <h1> People </h1>
       <ul>
-        {people.data.map((person) => (
+        {data.peopleAll.map((person) => (
           <li key={person.id}>{person.name}, living at {person.address}</li>
         ))}
       </ul>
+
+      <p>Partial select</p>
+      <ul>
+        {data.peopleMin.map(person => (
+          <li key={person.field1}>{person.field2}</li>
+        ))}
+      </ul>
+      
+      <p>First person ID {data.field1}, their name {data.field2}</p>
+      <p>Using arbitrary expressions as selection fields</p>
+      <ul>
+        {data.bartendersUpper.map(person => (
+          <li key={person.id}>{person.upperName}</li>
+        ))}
+      </ul>
+
       <p>
         <Link to="/addperson">
           Add a Person
