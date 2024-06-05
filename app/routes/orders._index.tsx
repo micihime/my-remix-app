d2c074edf829623878d794b2aced656bf5fd2033
Miči Hrešková <hroncovam@yahoo.com>
2024-06-05 15:11:21 +0100

    Add ordering of orders

--- app/routes/orders._index.tsx
+++ app/routes/orders._index.tsx
@@ -1,6 +1,6 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { Link, useLoaderData } from "@remix-run/react";
-import { and, eq, gte, lt, ne, not, sql } from "drizzle-orm";
+import { and, asc, desc, eq, gte, lt, ne, not, sql } from "drizzle-orm";
 
 import { db } from "~/drizzle/config.server";
 import { drinks, customers, orders } from "~/drizzle/schema.server";
@@ -10,7 +10,8 @@ export async function loader({ request, }: LoaderFunctionArgs) {
   const ordersAll = await db.select()
     .from(orders)
     .leftJoin(customers, eq(customers.id, orders.customerId))
-    .leftJoin(drinks, eq(drinks.id, orders.drinkId)) 
+    .leftJoin(drinks, eq(drinks.id, orders.drinkId))
+    .orderBy(asc(customers.id), desc(drinks.price))
 
   return json({
     ordersAll
@@ -19,13 +20,13 @@ export async function loader({ request, }: LoaderFunctionArgs) {
 
 export default function Items() {
   const data = useLoaderData<typeof loader>()
-
+  
   return (
     <div>
       <h1> Orders </h1>
       <ul>
         {data.ordersAll.map(order => (
-          <li key={order.customer_bartender_drinks.id}>{order.customers?.name} ordered {order.drinks?.name}</li>
+          <li key={order.customer_bartender_drinks.id}>{order.customers?.name} ordered {order.drinks?.name} for {order.drinks?.price}</li>
         ))}
       </ul>
 
