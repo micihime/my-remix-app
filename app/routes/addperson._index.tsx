1caf5a53cc64c3b30bc87490629a67e0a7428927
Miči Hrešková <hroncovam@yahoo.com>
2024-06-06 11:52:46 +0100

    Adding inputs for more properties of a person

--- app/routes/addperson._index.tsx
+++ app/routes/addperson._index.tsx
@@ -10,12 +10,34 @@ export default function AddPerson() {
     <div>
       <h1> Add a Person </h1>
       <Form method="post">
-        <p>
+        <div>
+          <label htmlFor="name">Name:</label>
           <input type="name" name="name" />
-        </p>
-        <p>
-          <input type="address" name="address" />
-        </p>
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input type="text" name="address" />
+        </div>
+        {/* <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" />
+        </div> */}
+        <div>
+          <label htmlFor="job">Job:</label>
+          <input type="text" name="job" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" name="phone" />
+        </div>
+        <div>
+          <label htmlFor="salary">Salary:</label>
+          <input type="number" name="salary" />
+        </div>
+        <div>
+          <label htmlFor="isSomething">Is this person something?</label>
+          <input type="radio" name="isSomething" />
+        </div>
         <button type="submit">Add</button>
       </Form>
     </div>
@@ -24,11 +46,13 @@ export default function AddPerson() {
 
 export async function action({ request, }: ActionFunctionArgs) {
   const formData = await request.formData();
+  console.log(formData)
+
   const name = String(formData.get("name"));
   const address = String(formData.get("address"));
   //console.log(name);
   
-  db.insert(people).values({ name: name, address: address }).run()
+  await db.insert(people).values({ name: name, address: address })
   
   return redirect("/people");
 }
