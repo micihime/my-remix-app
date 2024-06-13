2e06482f79a58de7703fcf9849a32572b141b98d
Miroslava Hreskova <hroncovam@yahoo.com>
2024-06-13 10:33:18 +0100

    Update signup._index.tsx

--- app/routes/signup._index.tsx
+++ app/routes/signup._index.tsx
@@ -1,8 +1,9 @@
-import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs, json } from "@remix-run/node";
+import { Form, Link, redirect, useLoaderData } from "@remix-run/react";
 import { sql } from "drizzle-orm";
 
 import { db } from "~/drizzle/config.server";
+import { userProfiles, users } from "~/drizzle/schema.server";
 
 export async function loader({ request, }: LoaderFunctionArgs) {
   // use drizzle to get the data
@@ -19,7 +20,41 @@ export default function People() {
   return (
     <div>
       <h1> People </h1>
-      
+      <Form method="post">
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input type="text" name="username" />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" name="name" />
+        </div>
+        <div>
+          <label htmlFor="address">Address:</label>
+          <input type="text" name="address" />
+        </div>
+        <div>
+          <label htmlFor="phone">Phone:</label>
+          <input type="tel" name="phone" />
+        </div>
+        <div>
+          <label htmlFor="job">Job:</label>
+          <input type="text" name="job" />
+        </div>
+        <div>
+          <label htmlFor="salary">Salary:</label>
+          <input type="number" name="salary" />
+        </div>
+        <div>
+          <label htmlFor="gender">Salary:</label>
+          <input type="text" name="gender" />
+        </div>
+        <button type="submit">Add</button>
+      </Form>
       <p>
         <Link to="/">
           Back Home
@@ -27,4 +62,25 @@ export default function People() {
       </p>
     </div>
   )
+}
+
+export async function action({ request, }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  //console.log(formData)
+
+  const username = String(formData.get("username"));
+  const email = String(formData.get("email"));
+  const name = String(formData.get("name"));
+  const address = String(formData.get("address"));
+  const phone = String(formData.get("phone"));
+  const job = String(formData.get("job"));
+  const salary = String(formData.get("salary"));
+  const gender = String(formData.get("gender"));
+
+  const test = await db.insert(users).values({ username: username, email: email })
+  console.log(test) //{ changes: 1, lastInsertRowid: 5 }
+  //const test2 = await db.insert(userProfiles).values({ username: username, email: email })
+  //console.log(test2) //{ changes: 1, lastInsertRowid: 5 }
+  
+  return redirect("/people");
 }
